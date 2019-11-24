a429cca22c3ac30f9f6a7adfd5c0ec74d8e31341
CasewardBen <48239811+CasewardBen@users.noreply.github.com>
2019-11-24 15:17:25 +0800

    初始化设置
    项目创建及初始化设置

---
+++ typeidea/typeidea/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for typeidea project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeidea.settings")
+profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
+os.environ.setdefault("DJANGO_SETTINGS_MODOULE", "typeidea.settings.%s" % profile)
+#修改了settings配置之后对settings文件路径的调整
+
+application = get_wsgi_application()
